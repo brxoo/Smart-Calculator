6313ad3aab4fe69fb58562e7073f7ea9bd94dcbd
brxoo <brunoaraujo198@gmail.com>
2021-07-12 14:35:26 -0300

    Declaração da variável resultadoEsperado

--- test/TestCalculadora.java
+++ test/TestCalculadora.java
@@ -9,6 +9,7 @@ public class TestCalculadora {
 	
 	double num1 = 25;
 	double num2 = 5;
+	double resultadoEsperado;
 	
 	private Calculadora calc;
 	
@@ -19,37 +20,37 @@ public class TestCalculadora {
 	
 	@Test
 	void testeSomar() {
-		double resultadoEsperado = num1 + num2;
+		resultadoEsperado = num1 + num2;
 		assertEquals(resultadoEsperado, calc.somar(num1, num2));
 	}
 	
 	@Test
 	void testeSubtrair() {
-		double resultadoEsperado = num1 - num2;
+		resultadoEsperado = num1 - num2;
 		assertEquals(resultadoEsperado, calc.subtrair(num1, num2));
 	}
 	
 	@Test
 	void testeMultiplicar() {
-		double resultadoEsperado = num1 * num2;
+		resultadoEsperado = num1 * num2;
 		assertEquals(resultadoEsperado, calc.multiplicar(num1, num2));
 	}
 	
 	@Test
 	void testeDividir() {
-		double resultadoEsperado = num1 / num2;
+		resultadoEsperado = num1 / num2;
 		assertEquals(resultadoEsperado, calc.dividir(num1, num2));
 	}
 	
 	@Test
 	void testeRaizQuadrada() {
-		double resultadoEsperado = Math.sqrt(num1);
+		resultadoEsperado = Math.sqrt(num1);
 		assertEquals(resultadoEsperado, calc.raizQuadrada(num1));
 	}
 	
 	@Test
 	void testePotencia() {
-		double resultadoEsperado = Math.pow(num1, num2);
+		resultadoEsperado = Math.pow(num1, num2);
 		assertEquals(resultadoEsperado, calc.potencia(num1, num2));
 	}
 }
